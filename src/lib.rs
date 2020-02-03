cd5fdd564096030b06cf463197a4f7343ba2913e
rusty-octopus <octopus@posteo.net>
2020-02-03 22:22:32 +0100

    Implemented first version of counting sort.

--- src/lib.rs
+++ src/lib.rs
@@ -26,20 +26,48 @@ impl CountingSort<u8> for Vec<u8> {
 
         let mut count_vector = count_values(&self, &min_value, &max_value, 0, 1);
 
+        prefix_sum(&mut count_vector);
+
+        // re-order
+        let mut sorted_vector:Vec<u8> = vec![0;self.len()];
+        for value in self.iter().rev() {
+            let index_count_vector = *value - min_value;
+            let mut index = count_vector[index_count_vector as usize];
+            index -= 1;
+            count_vector[index_count_vector as usize] = index;
+            sorted_vector[index as usize] = *value;
+        }
+
+        *self = sorted_vector;
     }
 
     fn counting_sort_known_min_max(&mut self, known_min_value: u8, known_max_value: u8){
     }
 }
 
+fn prefix_sum<T>(count_vector:&mut Vec<T>)
+    where T:Copy+Add<Output=T>
+{
+    let mut iterator = count_vector.iter_mut();
+    // skip first element
+    let first_element = iterator.next();
+    if first_element.is_some() {
+        let mut total = *first_element.unwrap();
+        for value in iterator {
+            total = total + *value;
+            *value = total;
+        }
+    }
+}
+
 fn count_values<T>(unsorted_vector:&Vec<T> /* impl Iterator? */, min_value:&T, max_value:&T, zero:T, one:T) -> Vec<T>
     where T: Ord+Copy+Sub<Output=T>+Add<Output=T>+Into<usize>
 {
-    let (min_value,max_value) = get_min_max(unsorted_vector, min_value, max_value);
+    let (min_value,max_value) = get_min_max_iter(unsorted_vector.iter(), &min_value, &max_value);
 
-    let length:usize = T::into(one + max_value - min_value);
+    let length:usize = T::into(one + *max_value - *min_value);
     let mut count_vector:Vec<<T as std::ops::Add>::Output> = vec![zero;length];
-    let offset = min_value;
+    let offset = *min_value;
 
     for value in unsorted_vector {
         let index = T::into(*value - offset);
@@ -80,6 +108,17 @@ mod tests {
     }
 
     #[test]
+    fn test_counting_sort() {
+        let mut test_vector = 
+            vec![13, 24, 27, 3, 10, 1, 9, 17, 6, 7, 3, 30, 14, 15, 2, 3, 7, 11, 21, 16, 7, 11, 21, 5, 23, 25, 26, 28, 28, 4];
+        test_vector.counting_sort();
+        let sorted_vector = 
+            vec![1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 7, 9, 10, 11, 11, 13, 14, 15, 16, 17, 21, 21, 23, 24, 25, 26, 27, 28, 28, 30];
+
+        assert_eq!(sorted_vector, test_vector);
+    }
+
+    #[test]
     fn test_unsigned_get_min_max() {
         let test_vector:Vec<u8> = vec![1,2,3,4];
         let (min_value,max_value) = get_min_max(&test_vector, &0, &255);
@@ -102,4 +141,19 @@ mod tests {
         assert_eq!(1,*min_value);
         assert_eq!(4,*max_value);
     }
+
+    #[test]
+    fn test_prefix_sum_1() {
+        let mut test_vector:Vec<u8> = vec![1;4];
+        prefix_sum(&mut test_vector);
+        assert_eq!(vec![1,2,3,4], test_vector);
+    }
+
+    #[test]
+    fn test_prefix_sum_2() {
+        let mut test_vector:Vec<u8> = vec![1,2,3,4,5];
+        prefix_sum(&mut test_vector);
+        assert_eq!(vec![1,3,6,10,15], test_vector);
+    }
+
 }
