9a9181553b45a345d7e12efb533adeb66164f7de
rusty-octopus <octopus@posteo.net>
2020-02-16 22:33:17 +0100

    Extracted re-order as function. Implemented it in 2 versions, iterator version is untested. Removed Add+Sub trait bound for count_vector function, since count_vector does only need to be usize.

--- src/lib.rs
+++ src/lib.rs
@@ -22,27 +22,59 @@ pub trait CountingSort<T> {
 
 impl CountingSort<u8> for Vec<u8> {
     fn counting_sort(&mut self){
-        let (min_value,max_value) = get_min_max(&self, &0, &255);
+        let (min_value,max_value) = get_min_max_iter(self.iter(), &&0, &&255);
 
-        let mut count_vector = count_values(&self, &min_value, &max_value, 0, 1);
+        let mut count_vector = count_values(&self, &min_value, &max_value);
 
         prefix_sum(&mut count_vector);
 
         // re-order
-        let mut sorted_vector:Vec<u8> = vec![0;self.len()];
-        for value in self.iter().rev() {
-            let index_count_vector = *value - min_value;
-            let mut index = count_vector[index_count_vector as usize];
-            index -= 1;
-            count_vector[index_count_vector as usize] = index;
-            sorted_vector[index as usize] = *value;
-        }
+        //let mut sorted_vector:Vec<u8> = vec![0;self.len()];
+        //for value in self.iter().rev() {
+        //    let index_count_vector = *value - min_value;
+        //    let mut index = count_vector[index_count_vector as usize];
+        //    index -= 1;
+        //    count_vector[index_count_vector as usize] = index;
+        //    sorted_vector[index as usize] = *value;
+        //}
+
+        let sorted_vector = re_order(&self, & mut count_vector, self.len(), min_value);
 
         *self = sorted_vector;
     }
 
     fn counting_sort_known_min_max(&mut self, known_min_value: u8, known_max_value: u8){
     }
+}
+
+fn re_order<T>(vector:&Vec<T>, count_vector:&mut Vec<usize>, length:usize, min_value:&T)-> Vec<T>
+    where T:Ord+Copy+Into<usize>+Sub<Output=T>
+{
+    let mut sorted_vector:Vec<T> = vec![*min_value; length];
+    for value in vector.iter().rev() {
+        let index_count_vector = T::into(*value - *min_value);
+        println!("index_count_vector: {}", index_count_vector);
+        let mut index =  count_vector[index_count_vector as usize];
+        index -= 1;
+        count_vector[index_count_vector as usize] = index;
+        sorted_vector[index as usize] = *value;
+    }
+    sorted_vector
+}
+
+fn re_order_iter<T,ITER>(iterator:ITER, count_vector:&mut Vec<usize>, length:usize, min_value:&T)-> Vec<T>
+    where T:Ord+Copy+Into<usize>+Sub<Output=T>+'static,
+          ITER:DoubleEndedIterator<Item=&'static T>
+{
+    let mut sorted_vector:Vec<T> = vec![*min_value; length];
+    for value in iterator.rev() {
+        let index_count_vector = T::into(*value - *min_value);
+        let mut index =  count_vector[index_count_vector as usize];
+        index -= 1;
+        count_vector[index_count_vector as usize] = index;
+        sorted_vector[index as usize] = *value;
+    }
+    sorted_vector
 }
 
 fn prefix_sum<T>(count_vector:&mut Vec<T>)
@@ -60,18 +92,18 @@ fn prefix_sum<T>(count_vector:&mut Vec<T>)
     }
 }
 
-fn count_values<T>(unsorted_vector:&Vec<T> /* impl Iterator? */, min_value:&T, max_value:&T, zero:T, one:T) -> Vec<T>
-    where T: Ord+Copy+Sub<Output=T>+Add<Output=T>+Into<usize>
+fn count_values<T>(unsorted_vector:&Vec<T> /* impl Iterator? */, min_value:&T, max_value:&T) -> Vec<usize>
+    where T: Ord+Copy+Into<usize>
 {
     let (min_value,max_value) = get_min_max_iter(unsorted_vector.iter(), &min_value, &max_value);
 
-    let length:usize = T::into(one + *max_value - *min_value);
-    let mut count_vector:Vec<<T as std::ops::Add>::Output> = vec![zero;length];
-    let offset = *min_value;
+    let offset = T::into(*min_value);
+    let length:usize = T::into(*max_value) - offset + 1;
+    let mut count_vector:Vec<usize> = vec![0;length];
 
     for value in unsorted_vector {
-        let index = T::into(*value - offset);
-        let new_count_value = count_vector[index] + one;
+        let index = T::into(*value) - offset;
+        let new_count_value = count_vector[index] + 1;
         count_vector[index] = new_count_value;
     }
 
