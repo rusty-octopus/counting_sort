3fe41412230deb1e784d5fd218a2b4c363f4c6e2
rusty-octopus <octopus@posteo.net>
2020-04-05 22:51:27 +0200

    Added integration tests. Identified integer overflow problem. Added IntoIndex and macro_rules for integers. Added unit tests for some i8 and u8 for IntoIndex. Commented out criterion to avoid time-consuming recompiles.

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,40 @@
 // https://en.wikipedia.org/wiki/Counting_sort
 
 // Todos:
-// 1. Only "link" against core not std (if possible)
+// 0. Tests for IntoIndex for Ints
+// 1. Integrate IntoIndex completely
+// 2. Re-execute benchmarks
+// 3. Tests: unit, component, docs
+//    * code coverage either via tarpaulin or kcov or both
+//    * i8, i16, i32, u8, u16, u32
+//    * test for lists, sets etc.
+//    * Integration tests for larger examples and using lists & vectors
+//    * Doc tests for all public methods
+// 4. Only "link" against core not std (if possible)
 //    * Change Error type so that std is no longer required
 //    * You need Vec currently, core is without any allocation afaik
 //    * You could only return a slice, but it would have to be created with a macro
-// 2. Tests: unit, component, docs
-//    * code coverage either via tarpaulin or kcov or both
-//    * i8, i16, i32, u8, u16, u32
-//    * Test for errors: e.g. when TryInto may fail
-//    * test for lists, sets etc.
-//    * Unit tests for all package private functions & all functions but with smaller examples
-//    * Integration tests for larger examples and using lists & vectors
-//    * Doc tests for all public methods
-// 3. Profile
-// 4. Optimizations
+// 5. Profile
+// 6. Optimizations
 //    * Copy elements into vector may result in less copies of the element
 //    * currently 2-3 copies per element due to TryInto
 //    * T:Clone instead of T copy?
-// 5. Analyze / Inspect / Evaluate, or add more errors + 2 versions (abort when too much memory or execute anyway)
+// 7. Analyze / Inspect / Evaluate, or add more errors + 2 versions (abort when too much memory or execute anyway)
 //    * Used memory and runtime
-// 6. Publish?
+// 8. Move benchmark into own library due to long build and test times
+// 9. Publish?
 
 use core::cmp::{max, min, Ord};
 use core::convert::TryInto;
 use core::fmt;
 use core::fmt::Display;
-use core::ops::Sub;
+use core::ops::{Sub};
 use std::error::Error;
 
 #[derive(Debug)]
 pub enum CountingSortError {
     IntoUsizeError(&'static str),
+    MinValueLargerThanValue(&'static str),
     IteratorEmpty(&'static str),
     SortingUnnecessary(&'static str),
 }
@@ -45,6 +48,7 @@ impl Display for CountingSortError {
                 "Error from TryInto<usize>. Original error message: {}.",
                 description
             ),
+            CountingSortError::MinValueLargerThanValue(description) => description.fmt(f),
             CountingSortError::IteratorEmpty(description) => description.fmt(f),
             CountingSortError::SortingUnnecessary(description) => description.fmt(f),
         }
@@ -58,13 +62,17 @@ impl CountingSortError {
         CountingSortError::IntoUsizeError("Out of range integral type conversion attempted")
     }
 
+    fn from_min_value_larger_than_value() -> CountingSortError {
+        CountingSortError::MinValueLargerThanValue("Given min_value is larger than value")
+    }
+
     fn from_empty_iterator() -> CountingSortError {
         CountingSortError::IteratorEmpty("There are no element available in the iterator")
     }
 
     fn from_sorting_unnecessary() -> CountingSortError {
         CountingSortError::SortingUnnecessary(
-            "Minimum value is identical to maximum value. Therefore no sorting is necessary",
+            "Minimum value is identical to maximum value, therefore no sorting is necessary",
         )
     }
 }
@@ -89,6 +97,74 @@ where
     ITER: Sized + DoubleEndedIterator<Item = &'a T> + Clone,
 {
 }
+
+pub trait TryIntoIndex {
+    type Error;
+    fn try_into_index(value:&Self, min_value:&Self) -> Result<usize, Self::Error>; 
+}
+
+macro_rules! try_into_index_impl_for_integers {
+    ($smaller_int:ty,$larger_int:ty) => {
+        impl TryIntoIndex for $smaller_int {
+            type Error = CountingSortError;
+
+            fn try_into_index(value: &Self, min_value: &Self) -> Result<usize, Self::Error> {
+                if min_value <= value {
+                    let result = <$larger_int>::try_into(<$larger_int>::from(*value) - <$larger_int>::from(*min_value));
+                    if result.is_err() {
+                        Err(CountingSortError::from_try_into_error())
+                    } else {
+                        Ok(result.unwrap_or(0))
+                    }
+                } else {
+                    Err(CountingSortError::from_min_value_larger_than_value())
+                }
+            }
+        }
+    };
+}
+
+try_into_index_impl_for_integers!(i8,i16);
+try_into_index_impl_for_integers!(i16,i32);
+try_into_index_impl_for_integers!(i32,i64);
+try_into_index_impl_for_integers!(u8,u16);
+try_into_index_impl_for_integers!(u16,u32);
+try_into_index_impl_for_integers!(u32,u64);
+
+
+//impl TryIntoIndex for i8 {
+//    type Error = <i8 as std::convert::TryInto<usize>>::Error;
+//
+//    fn try_into_index(value:&Self, min_value:&Self) -> Result<usize, Self::Error> {
+//        if min_value <= value {
+//            i16::try_into(i16::from(*value) - i16::from(*min_value))
+//        } else {
+//            panic!("Implement this error correctly")
+//        }
+//    }
+//}
+
+
+// impl<U> TryIntoIndex for TryIntoIndexInteger<U>
+// where
+//     U: From<T> + Sub<Output=U> + TryInto<usize>
+// {
+//     type Error = <U as std::convert::TryInto<usize>>::Error;
+
+//     fn try_into_index(value:&Self, min_value:&Self) -> Result<usize, Self::Error> {
+//         U::try_into(U::from(*value) - U::from(*min_value))
+//         //if min_value <= value {
+//         //    let result = U::try_into(U::from(*value) - U::from(*min_value));
+//         //    if result.is_ok() {
+//         //        Ok(result.unwrap())
+//         //    } else {
+//         //        Err(String::from("Implement correct error type here."))
+//         //    }
+//         //} else {
+//         //    panic!("Implement this error correctly")
+//         //}
+//     }
+// }
 
 fn counting_sort<'a, ITER, T>(iterator: ITER) -> Result<Vec<T>, CountingSortError>
 where
@@ -164,6 +240,17 @@ where
 {
     // max_value - min_value should always be >= "0"
     // however it could overflow usize
+    // and it will overflow if max_value - min_value > i*::max_value
+    // FIXME: function that avoids overflows
+    //  * usage similar to https://crates.io/crates/num-traits CheckedSub?
+    //  * Use Wrapping? => I'll guess no, because I want 127 - -128 to be 255 obviously
+    //  * Always convert to a larger integer for the sub? LIke T1, T2, with T2: Sub<Output=T2> and T1: Into<T1>
+    //  * Identify all overflwoing or problematic cases
+    //  * Is there a straight-forward solution when it overflows? 
+    //      * E.g. convert to one larger type integer?
+    //      * or use max_value for these cases to "know" what to do?
+    //  * Use an own trait, which implements "get_index" for all integer types
+    //      * Provide a "blanket" implementation for types that easily convert to integertypes
     let length = T::try_into(*max_value - *min_value)? + 1;
     let mut count_vector: Vec<usize> = vec![0; length];
 
@@ -242,6 +329,13 @@ mod unit_tests {
     }
 
     #[test]
+    fn test_cnt_sort_i8_vector_with_overflow() {
+        let test_vector: Vec<i8> = vec![2, -100, 50, -6];
+        let sorted_vector = counting_sort(test_vector.iter()).unwrap();
+        assert_eq!(vec![-6, -2, 1, 2], sorted_vector);
+    }
+
+    #[test]
     fn test_cnt_sort_u8_vector() {
         let mut test_vector = TEST_ARRAY_UNSORTED.to_vec();
         test_vector = test_vector.iter().cnt_sort().unwrap();
@@ -258,6 +352,30 @@ mod unit_tests {
             .unwrap();
         let sorted_vector = TEST_ARRAY_SORTED.to_vec();
         assert_eq!(sorted_vector, test_vector);
+    }
+
+    #[test]
+    fn test_into_index_i8() {
+        assert_eq!(255, i8::try_into_index(&127,&-128).unwrap());
+        assert_eq!(0, i8::try_into_index(&-128,&-128).unwrap());
+        assert_eq!(150, i8::try_into_index(&50,&-100).unwrap());
+        assert_eq!(50, i8::try_into_index(&-50,&-100).unwrap());
+        assert_eq!(27, i8::try_into_index(&127,&100).unwrap());
+    }
+
+    #[test]
+    fn test_into_index_u8() {
+        assert_eq!(255, u8::try_into_index(&255,&0).unwrap());
+        assert_eq!(0, u8::try_into_index(&0,&0).unwrap());
+        assert_eq!(50, u8::try_into_index(&150,&100).unwrap());
+        assert_eq!(50, u8::try_into_index(&100,&50).unwrap());
+        assert_eq!(27, i8::try_into_index(&127,&100).unwrap());
+    }
+
+    #[test]
+    fn test_into_index_error() {
+        let error = i8::try_into_index(&-127,&127);
+        assert_eq!("Given min_value is larger than value", format!("{}", error.unwrap_err()));
     }
 
     #[test]
@@ -364,7 +482,7 @@ mod unit_tests {
         let result = counting_sort_min_max(test_vector.iter(), &1, &1);
         assert!(result.is_err());
         assert_eq!(
-            "Minimum value is identical to maximum value. Therefore no sorting is necessary",
+            "Minimum value is identical to maximum value, therefore no sorting is necessary",
             format!("{}", result.unwrap_err())
         );
     }
@@ -443,4 +561,5 @@ mod unit_tests {
         assert!(result.is_ok());
         assert_eq!(test_vector, result.unwrap());
     }
+
 }
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -9,7 +9,7 @@ use core::ops::Range;
 use core::convert::TryFrom;
 
 #[test]
-fn test_counting_sort_on_list() {
+fn test_with_list() {
     let mut list = LinkedList::new();
     list.push_back(4);
     list.push_back(3);
@@ -21,19 +21,19 @@ fn test_counting_sort_on_list() {
 }
 
 #[test]
-fn test_counting_sort_on_list_2() {
-    let mut list = LinkedList::new();
-    list.push_back(4);
-    list.push_back(3);
-    list.push_back(2);
-    list.push_back(1);
-    let sorted_vector = list.iter().cnt_sort().unwrap();
+fn test_with_vector() {
+    let mut vector = Vec::new();
+    vector.push(4);
+    vector.push(3);
+    vector.push(2);
+    vector.push(1);
+    let sorted_vector = vector.iter().cnt_sort().unwrap();
     let test_vector = vec![1, 2, 3, 4];
     assert_eq!(test_vector, sorted_vector);
 }
 
 #[test]
-fn test_slice() {
+fn test_with_slice() {
     let slice: [u8; 4] = [4, 3, 2, 1];
     let sorted_vector = slice.iter().cnt_sort().unwrap();
     let test_vector = vec![1, 2, 3, 4];
@@ -42,7 +42,7 @@ fn test_slice() {
 
 #[cfg(test)]
 #[cfg_attr(tarpaulin, skip)]
-fn create_vector_t<T: TryFrom<u32>>(number_of_elements: usize, range: Range<u32>) -> Vec<T> {
+fn create_vector_t_unsigned<T: TryFrom<u32>>(number_of_elements: usize, range: Range<u32>) -> Vec<T> {
     let mut vector: Vec<T> = Vec::with_capacity(number_of_elements);
     let mut rng = Rand32::new(7648730752358173238);
     for _ in 0..number_of_elements {
@@ -56,14 +56,66 @@ fn create_vector_t<T: TryFrom<u32>>(number_of_elements: usize, range: Range<u32>
     vector
 }
 
+#[cfg(test)]
+#[cfg_attr(tarpaulin, skip)]
+fn convert_to_signed(value:u32) -> i32 {
+    let max_i32 =  u32::max_value() / 2;
+    if value > u32::max_value() / 2 {
+        i32::min_value() + (value - max_i32) as i32
+    } else {
+        value as i32
+    }
+}
+
+#[cfg(test)]
+#[cfg_attr(tarpaulin, skip)]
+fn create_vector_t_signed<T: TryFrom<i32>>(number_of_elements: usize, range: Range<u32>) -> Vec<T> {
+    let mut vector: Vec<T> = Vec::with_capacity(number_of_elements);
+    let mut rng = Rand32::new(7648730752358173238);
+    for _ in 0..number_of_elements {
+        let random_u32 = rng.rand_range(range.clone());
+        let random_value_result = T::try_from( convert_to_signed(random_u32));
+        match random_value_result {
+            Ok(v) => vector.push(v),
+            Err(_) => println!("Error occurred converting {}", random_u32),
+        };
+    }
+    vector
+}
+
 #[test]
-fn test_cnt_sort_min_max_on_u8_vector() {
+fn test_with_vector_u8_10k() {
     let number_of_elements = 10000;
     let range_min: u32 = 0;
     let range_max: u32 = 256;
-    let vector = create_vector_t::<u8>(number_of_elements, range_min..range_max);
+    let mut vector = create_vector_t_unsigned::<u8>(number_of_elements, range_min..range_max);
     let result = vector
         .iter()
-        .cnt_sort_min_max(&(range_min as u8), &(range_max as u8));
-    assert!(result.is_err());
+        .cnt_sort_min_max(&(range_min as u8), &255);
+    assert!(result.is_ok());
+    vector.sort();
+    assert_eq!(vector, result.unwrap());
+}
+
+#[test]
+fn test_with_vector_i32_10k() {
+    let number_of_elements = 10000;
+    let range_min: u32 = 0;
+    let range_max: u32 = u32::max_value();
+    let mut vector = create_vector_t_signed::<i32>(number_of_elements, range_min..range_max);
+    let neg_vector = vector.iter().filter(|x| x < &&0).map(|x|x.clone()).collect::<Vec<i32>>();
+    assert!(neg_vector.len() > 0);
+    let result = vector
+        .iter()
+        .cnt_sort();
+    assert!(result.is_ok());
+    vector.sort();
+    assert_eq!(vector, result.unwrap());
+}
+
+#[test]
+fn test_does_not_panic_with_signed() {
+    let vector:Vec<i8> = vec![-128, 127];
+    let sorted_vector = vector.iter().cnt_sort().unwrap();
+    assert_eq!(vector, sorted_vector);
 }
